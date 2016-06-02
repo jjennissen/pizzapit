476c22d51103ef54265884bc6d1a9bfc144f3f05
jjennissen <jjennissen@users.noreply.github.com>
2016-06-02 18:46:38 -0500

    Add files via upload

---
+++ Scripts/GunFire.js
@@ -0,0 +1,7 @@
+﻿function Update() {
+    if (Input.GetButtonDown("Fire1")) {
+        var gunsound : AudioSource = GetComponent.<AudioSource>();
+        gunsound.Play();
+        GetComponent.<Animation>().Play("GunShot");
+    }
+}
